c6c23a85cc416c86ee8989bc6e42b2777a887219
Konstantin Kayukin <t_kkayukin@admarketplace.com>
2026-02-19 13:37:07 +0100

    logs

--- service.sh
+++ service.sh
@@ -1,10 +1,25 @@
 #!/system/bin/sh
-until [ "$(getprop sys.boot_completed)" = "1" ]; do
-  sleep 5
+while [ "$(getprop sys.boot_completed)" != 1 ]; do
+    sleep 1
 done
-
-sleep 5
+log Info "System loaded"
+sleep 20
+log Info "Started mounting"
 
 mount -t tmpfs -o mode=755 tmpfs /sys/fs/cgroup
 mkdir -p /sys/fs/cgroup/devices
 mount -t cgroup -o devices cgroup /sys/fs/cgroup/devices
+
+log Info "Finished mounting"
+
+ip route add default via 192.168.1.1 dev wlan0
+ip rule add from all lookup main pref 30000
+
+log Info "Routes added"
+
+ndc network interface add local docker0
+ndc network route add local docker0 172.17.0.0/16
+ndc ipfwd enable docker
+ndc ipfwd add docker0 wlan0
+
+log Info "Bridge fixed"
